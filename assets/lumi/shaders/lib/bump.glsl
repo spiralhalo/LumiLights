b6e18b93bac28fa66b62c09e18005834ec0170d0
spiralhalo <re.nanashi95@gmail.com>
2020-12-06 03:12:13 +0700

    Code readability

--- assets/lumi/shaders/lib/bump.glsl
+++ assets/lumi/shaders/lib/bump.glsl
@@ -39,9 +39,14 @@ vec3 bump_normal(sampler2D tex, vec3 normal, vec2 uvn, vec2 uvt, vec2 uvb)
     vec3 tangentMove = _bump_tangentMove(normal);
     vec3 bitangentMove = _bump_bitangentMove(normal, tangentMove);
 
-    vec3 origin = _bump_height(frx_luminance(texture2D(tex, uvn, _cv_getFlag(_CV_FLAG_UNMIPPED) * -4.0).rgb))*normal;
-    vec3 tangent = tangentMove + _bump_height(frx_luminance(texture2D(tex, uvt, _cv_getFlag(_CV_FLAG_UNMIPPED) * -4.0).rgb))*normal - origin;
-    vec3 bitangent = bitangentMove + _bump_height(frx_luminance(texture2D(tex, uvb, _cv_getFlag(_CV_FLAG_UNMIPPED) * -4.0).rgb))*normal - origin;
+    vec4 texel     = texture2D(tex, uvn, _cv_getFlag(_CV_FLAG_UNMIPPED) * -4.0);
+    vec3 origin    = _bump_height(frx_luminance(texel.rgb)) * normal;
+
+         texel     = texture2D(tex, uvt, _cv_getFlag(_CV_FLAG_UNMIPPED) * -4.0);
+    vec3 tangent   = tangentMove + _bump_height(frx_luminance(texel.rgb)) * normal - origin;
+    
+         texel     = texture2D(tex, uvb, _cv_getFlag(_CV_FLAG_UNMIPPED) * -4.0);
+    vec3 bitangent = bitangentMove + _bump_height(frx_luminance(texel.rgb)) * normal - origin;
 
     return normalize(cross(tangent, bitangent));
 }
