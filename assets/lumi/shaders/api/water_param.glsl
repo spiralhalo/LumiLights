b7d805b4b782ff2e7ce63b419f89c0fb525c916a
spiralhalo <re.nanashi95@gmail.com>
2020-12-16 03:12:04 +0700

    Water param api (derp ultimate)

---
+++ assets/lumi/shaders/api/water_param.glsl
@@ -0,0 +1,7 @@
+const float waveSpeed = 1;
+const float scale = 1.5;
+const float amplitude = 0.01;
+// const float texAmplitude = 0.005;
+const float stretch = 2;
+// wave movement doesn't necessarily follow flow direction for the time being
+const vec3 moveSpeed = vec3(0.5, 1.5, -0.5);
