1453bb786e54158adc0d1d64145121405803bb4a
spiralhalo <re.nanashi95@gmail.com>
2020-12-30 07:36:04 +0700

    Code reorg 5

--- assets/lumi/shaders/internal/lightsource.glsl
+++ assets/lumi/shaders/internal/lightsource.glsl
@@ -14,32 +14,33 @@
 #endif
 
 #ifdef LUMI_PBRX
-#define hdr_sunStr 5
-#define hdr_moonStr 0.4
-#define hdr_blockMinStr 2
-#define hdr_blockMaxStr 3
-#define hdr_handHeldStr 1.5
-#define hdr_skylessStr 0.1
-#define hdr_baseMinStr 0.01
-#define hdr_baseMaxStr 0.8
-#define hdr_emissiveStr 1
-#define hdr_relAmbient toneAdjust(0.2)
-#define hdr_dramaticStr 1.0
-#define hdr_dramaticMagicNumber 6.0
+	#define hdr_sunStr 5
+	#define hdr_moonStr 0.4
+	#define hdr_blockMinStr 2
+	#define hdr_blockMaxStr 3
+	#define hdr_handHeldStr 1.5
+	#define hdr_skylessStr 0.1
+	#define hdr_baseMinStr 0.01
+	#define hdr_baseMaxStr 0.8
+	#define hdr_emissiveStr 1
+	#define hdr_relAmbient toneAdjust(0.2)
+	#define hdr_dramaticStr 1.0
+	#define hdr_dramaticMagicNumber 6.0
 #else
-#define hdr_sunStr 1.8
-#define hdr_moonStr 0.18
-#define hdr_blockMinStr 1.0
-#define hdr_blockMaxStr 1.4
-#define hdr_handHeldStr 0.9
-#define hdr_skylessStr 0.05
-#define hdr_baseMinStr 0.0
-#define hdr_baseMaxStr 0.25
-#define hdr_emissiveStr 1
-#define hdr_relAmbient toneAdjust(0.09)
-#define hdr_dramaticStr 0.6
-#define hdr_dramaticMagicNumber 3.5
+	#define hdr_sunStr 1.8
+	#define hdr_moonStr 0.18
+	#define hdr_blockMinStr 1.0
+	#define hdr_blockMaxStr 1.4
+	#define hdr_handHeldStr 0.9
+	#define hdr_skylessStr 0.05
+	#define hdr_baseMinStr 0.0
+	#define hdr_baseMaxStr 0.25
+	#define hdr_emissiveStr 1
+	#define hdr_relAmbient toneAdjust(0.09)
+	#define hdr_dramaticStr 0.6
+	#define hdr_dramaticMagicNumber 3.5
 #endif
+
 #define hdr_nightAmbientMult 2.0
 #define hdr_skylessRelStr 0.5
 #define hdr_zWobbleDefault 0.1
@@ -65,16 +66,18 @@ const vec3 nvColor = vec3(0.63, 0.55, 0.64);
 #ifndef LUMI_DayAmbientBlue
 	#define LUMI_DayAmbientBlue 10
 #endif
-const vec3 preAmbient = hdr_gammaAdjust(mix(vec3(0.8550322), vec3(0.6, 0.9, 1.0), clamp(LUMI_DayAmbientBlue * 0.1, 0.0, 1.0))) * hdr_sunStr;
-const vec3 preBlueAmbient = hdr_gammaAdjust(vec3(0.6, 0.9, 1.0)) * hdr_sunStr;
+
+#define preAmbient hdr_gammaAdjust(mix(vec3(0.8550322), vec3(0.6, 0.9, 1.0), clamp(LUMI_DayAmbientBlue * 0.1, 0.0, 1.0))) * hdr_sunStr
+#define preBlueAmbient hdr_gammaAdjust(vec3(0.6, 0.9, 1.0)) * hdr_sunStr
+
 #if LUMI_LightingMode == LUMI_LightingMode_Dramatic
-const vec3 preSunriseAmbient = hdr_gammaAdjust(vec3(0.5, 0.3, 0.1)) * hdr_sunStr;
-const vec3 preSunsetAmbient = hdr_gammaAdjust(vec3(0.5, 0.2, 0.0)) * hdr_sunStr;
-const vec3 preNightAmbient = hdr_gammaAdjust(vec3(0.74, 0.4, 1.0)) * hdr_moonStr * hdr_nightAmbientMult;
+	#define preSunriseAmbient hdr_gammaAdjust(vec3(0.5, 0.3, 0.1)) * hdr_sunStr
+	#define preSunsetAmbient hdr_gammaAdjust(vec3(0.5, 0.2, 0.0)) * hdr_sunStr
+	#define preNightAmbient hdr_gammaAdjust(vec3(0.74, 0.4, 1.0)) * hdr_moonStr * hdr_nightAmbientMult
 #else
-const vec3 preSunriseAmbient = hdr_gammaAdjust(vec3(1.0, 0.8, 0.4)) * hdr_sunStr;
-const vec3 preSunsetAmbient = hdr_gammaAdjust(vec3(1.0, 0.6, 0.2)) * hdr_sunStr;
-const vec3 preNightAmbient = hdr_gammaAdjust(vec3(0.5, 0.5, 1.0)) * hdr_moonStr * hdr_nightAmbientMult;
+	#define preSunriseAmbient hdr_gammaAdjust(vec3(1.0, 0.8, 0.4)) * hdr_sunStr
+	#define preSunsetAmbient hdr_gammaAdjust(vec3(1.0, 0.6, 0.2)) * hdr_sunStr
+	#define preNightAmbient hdr_gammaAdjust(vec3(0.5, 0.5, 1.0)) * hdr_moonStr * hdr_nightAmbientMult
 #endif
 
 /*  BLOCK LIGHT
