30ae8f10183132a2f59413beaf79e89c5198c6c5
spiralhalo <re.nanashi95@gmail.com>
2021-01-15 20:19:59 +0700

    Pretty good filmic tonemap methinks

--- assets/lumi/shaders/lib/tonemap.glsl
+++ assets/lumi/shaders/lib/tonemap.glsl
@@ -19,15 +19,37 @@ vec3 ldr_vibrantTonemap(in vec3 hdrColor){
 }
 #endif
 
+vec3 hable_tonemap_partial(vec3 x)
+{
+    float A = 0.15f;
+    float B = 0.50f;
+    float C = 0.10f;
+    float D = 0.20f;
+    float E = 0.02f;
+    float F = 0.30f;
+    return ((x*(A*x+C*B)+D*E)/(x*(A*x+B)+D*F))-E/F;
+}
+
+vec3 hable_filmic(vec3 v)
+{
+    float exposure_bias = 2.0f;
+    vec3 curr = hable_tonemap_partial(v * exposure_bias);
+
+    vec3 W = vec3(11.2f);
+    vec3 white_scale = vec3(1.0f) / hable_tonemap_partial(W);
+    return curr * white_scale;
+}
+
 vec4 ldr_tonemap(vec4 a) {
     vec3 c = a.rgb;
-#if LUMI_Tonemap == LUMI_Tonemap_Film
-    c = frx_toneMap(c);
-#elif LUMI_Tonemap == LUMI_Tonemap_Vibrant
-    c = ldr_vibrantTonemap(c);
-#else
-    c = ldr_reinhardJodieTonemap(c);
-#endif
+    c = hable_filmic(c);
+// #if LUMI_Tonemap == LUMI_Tonemap_Film
+//     c = frx_toneMap(c);
+// #elif LUMI_Tonemap == LUMI_Tonemap_Vibrant
+//     c = ldr_vibrantTonemap(c);
+// #else
+//     c = ldr_reinhardJodieTonemap(c);
+// #endif
     // Somehow the film tonemap requires clamping. I don't understand..
     c = pow(clamp(c, 0.0, 1.0), vec3(1.0 / hdr_gamma));
     return vec4(c, a.a);
