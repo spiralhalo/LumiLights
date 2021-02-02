cdb0a15a92a55af0a9867f6876f109c38928cad2
spiralhalo <re.nanashi95@gmail.com>
2021-02-02 23:06:35 +0700

    Trailing roughness fade (might be pointless)

--- assets/lumi/shaders/post/reflection_common.glsl
+++ assets/lumi/shaders/post/reflection_common.glsl
@@ -61,8 +61,9 @@ vec3 pbr_lightCalc(float roughness, vec3 f0, vec3 radiance, vec3 lightDir, vec3 
 {
 	vec3 halfway = normalize(viewDir + lightDir);
 	vec3 fresnel = pbr_fresnelSchlick(pbr_dot(viewDir, halfway), f0);
+    float roughnessFade = smoothstep(REFLECTION_MAXIMUM_ROUGHNESS, REFLECTION_MAXIMUM_ROUGHNESS - 0.05, roughness);
 
-    return clamp(fresnel * radiance * (1-roughness), 0.0, 1.0);
+    return clamp(fresnel * radiance * roughnessFade * (1-roughness), 0.0, 1.0);
 }
 
 rt_Result rt_reflection(
