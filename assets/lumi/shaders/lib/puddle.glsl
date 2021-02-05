bccedaf4d503a940a5e9f6d2211f1b3344b8f15d
spiralhalo <re.nanashi95@gmail.com>
2021-02-06 00:41:47 +0700

    Slight optimizations, adjust puddles

--- assets/lumi/shaders/lib/puddle.glsl
+++ assets/lumi/shaders/lib/puddle.glsl
@@ -8,13 +8,11 @@
  *******************************************************/
 
 void ww_puddle_pbr(inout vec4 a, inout float roughness, in float light_y, inout vec3 normal, in vec3 worldPos) {
-    if (frx_modelOriginType() != MODEL_ORIGIN_REGION) return;
-    float wet = (light_y >= 0.9 ? 1.0 : 0.0)
-                   * (0.5 + 0.5 * snoise(0.1 * worldPos.xz))
-                   * smoothstep(0.0, 1.0, normal.y)
-                   * frx_rainGradient();
-    wet = 0.5 * frx_rainGradient() + 0.5 * smoothstep(0.3, 0.9, wet);
-    float puddly = smoothstep(0.9, 1.0, wet);
+    float rainExposure = (light_y >= 0.93 ? 1.0 : 0.0) * frx_rainGradient() * smoothstep(0.0, 1.0, normal.y);
+    if (rainExposure == 0.0 || frx_modelOriginType() != MODEL_ORIGIN_REGION) return;
+    float wet = rainExposure * (0.5 + 0.5 * snoise(0.1 * worldPos.xz));
+    float puddly = smoothstep(0.7, 0.8, wet);
+    wet = 0.5 * frx_rainGradient() + 0.5 * smoothstep(0.1, 0.7, wet);
     roughness = min(roughness, 1.0 - 0.9 * (wet * 0.5 + 0.5 * puddly));
     a.rgb *= 1.0 - 0.3 * wet;
     vec3 mov = vec3(0.0, frx_renderSeconds() * 6.0, 0.0);
