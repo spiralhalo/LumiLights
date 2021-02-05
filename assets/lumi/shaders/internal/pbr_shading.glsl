0096094c94c4ea3bfb9604258f4be89554c180be
spiralhalo <re.nanashi95@gmail.com>
2021-02-05 09:43:03 +0700

    Test ambient reflection

--- assets/lumi/shaders/internal/pbr_shading.glsl
+++ assets/lumi/shaders/internal/pbr_shading.glsl
@@ -62,6 +62,12 @@ struct light_data{
     vec3 specularAccu;
 };
 
+vec3 hdr_calcAmbientLight(inout light_data data, vec3 radiance)
+{
+    vec3 ambientReflection = pbr_fresnelSchlick(pbr_dot(data.viewDir, data.normal), data.f0 * data.metallic) * (1.0 - data.roughness);
+    return data.albedo * pbr_fakeMetallicDiffuseMultiplier(data.albedo, data.metallic, radiance) / PI * radiance * (1.0 - ambientReflection) + ambientReflection * radiance;
+}
+
 vec3 hdr_calcBlockLight(inout light_data data, vec3 radiance)
 {
     if (data.diffuse) {
@@ -91,7 +97,7 @@ vec3 hdr_calcSkyAmbientLight(inout light_data data)
     if (frx_worldHasSkylight())
     {
         vec3 skyRadiance = l2_skyAmbient(data.light.y, frx_worldTime(), frx_ambientIntensity());
-        return pbr_nonDirectional(data.albedo, data.metallic, skyRadiance);
+        return hdr_calcAmbientLight(data, skyRadiance);
     }
     return vec3(0.0);
 }
@@ -162,7 +168,7 @@ void pbr_shading(in frx_FragmentData fragData, inout vec4 a, inout float bloom, 
     float ao = l2_ao(fragData);
     vec3 held_light = hdr_calcHeldLight(data);
     vec3 block_light = hdr_calcBlockLight(data, l2_blockRadiance(data.light.x));
-    vec3 base_ambient_light = pbr_nonDirectional(data.albedo, data.metallic, l2_baseAmbient());
+    vec3 base_ambient_light = hdr_calcAmbientLight(data, l2_baseAmbient());
     vec3 sky_ambient_light = hdr_calcSkyAmbientLight(data);
     vec3 sky_light = hdr_calcSkyLight(data);
     vec3 emissive_light = pbr_nonDirectional(data.albedo, data.metallic, l2_emissiveRadiance(data.albedo, bloom));
