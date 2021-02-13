bd9b6da5a01d0b04fe293af0f0eb3785f5b37e86
spiralhalo <re.nanashi95@gmail.com>
2021-02-13 23:59:42 +0700

    Sky vector fix, color adjust, render basic sun/moon

--- assets/lumi/shaders/context/global/lighting.glsl
+++ assets/lumi/shaders/context/global/lighting.glsl
@@ -54,7 +54,11 @@ const vec3 NETHER_SKYLESS_LIGHT_COLOR = vec3(1.0, 0.5, 0.3);
 // SKY COLORS
 // const vec3 SUNRISE_SKY_COLOR = vec3(0.5, 0.3, 0.1);
 const vec3 ORANGE_SKY_COLOR = vec3(0.5, 0.2, 0.0);
+#ifdef CUSTOM_SKY
+const vec3 NIGHT_SKY_COLOR = vec3(0.05, 0.05, 0.3);
+#else
 const vec3 NIGHT_SKY_COLOR = vec3(0.01, 0.01, 0.01);
+#endif
 const vec3 DAY_SKY_COLOR = vec3(0.52, 0.69, 1.0);
 
 // GAMMA-ADJUSTED AMBIENT
