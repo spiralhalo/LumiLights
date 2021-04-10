21eecbd6eca79f52e6a83442011491bfd0cb308b
spiralhalo <re.nanashi95@gmail.com>
2021-04-10 16:35:45 +0700

    Whoops

--- assets/lumi/shaders/common/compat.glsl
+++ assets/lumi/shaders/common/compat.glsl
@@ -6,7 +6,7 @@
 // NOT compatibility with GLSL 1.2
 
 #if __VERSION__ == 130
-#define frx_guiViewProjectionMatrix() gl_ModelViewMatrix * gl_ProjectionMatrix
+#define frx_guiViewProjectionMatrix() gl_ProjectionMatrix * gl_ModelViewMatrix
 #define frxu_frameProjectionMatrix gl_ProjectionMatrix
 #define in_vertex gl_Vertex
 #endif
