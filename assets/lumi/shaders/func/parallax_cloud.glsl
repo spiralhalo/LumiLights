058029ddb08008526137e108cc63db2da2e2ef76
spiralhalo <re.nanashi95@gmail.com>
2021-05-16 15:36:18 +0700

    Cheaper parallax clouds

--- assets/lumi/shaders/func/parallax_cloud.glsl
+++ assets/lumi/shaders/func/parallax_cloud.glsl
@@ -33,7 +33,7 @@ vec4 parallaxCloud(in sampler2D sbluenoise, in vec2 texcoord)
         return vec4(0.);
     }
     
-    const int flatLoop = 6;
+    const int flatLoop = 3;
     const float flatMult = 1./float(flatLoop);
     const float CLOUD_ALTITUDE  = PARALLAX_CLOUD_ALTITUDE;
     const float CLOUD_THICKNESS = PARALLAX_CLOUD_THICKNESS;
@@ -61,7 +61,7 @@ vec4 parallaxCloud(in sampler2D sbluenoise, in vec2 texcoord)
         localCloud = l2_clampScale(0.15, 0.45, cloud1);
         localCloud *= flatMult * PARALLAX_CLOUD_DENSITY;
 
-        float topness = float(i) * flatMult * 0.6 + 0.4;
+        float topness = float(i) * flatMult;
 
         vec3 localColor = ldr_tonemap3(atmos_hdrCelestialRadiance() * 0.2) * topness + ldr_tonemap3(atmos_hdrSkyColorRadiance(worldSkyVec) * 0.3);
         if (i == flatLoop) {
