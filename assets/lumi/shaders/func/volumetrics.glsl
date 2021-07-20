a129bfed798dd23e263d6de0a97773261bc3629f
spiralhalo <re.nanashi95@gmail.com>
2021-07-20 10:54:19 +0700

    Librarify caustics

---
+++ assets/lumi/shaders/func/volumetrics.glsl
@@ -0,0 +1,76 @@
+#include frex:shaders/api/world.glsl
+#include lumi:shaders/common/shadow.glsl
+#include lumi:shaders/lib/caustics.glsl
+
+/*******************************************************
+ *  lumi:shaders/func/volumetrics.glsl                 *
+ *******************************************************
+ *  Copyright (c) 2020-2021 spiralhalo                 *
+ *  Released WITHOUT WARRANTY under the terms of the   *
+ *  GNU Lesser General Public License version 3 as     *
+ *  published by the Free Software Foundation, Inc.    *
+ *******************************************************/
+
+vec4 underwaterLightRays(sampler2DArrayShadow sshadow, vec4 a, vec3 modelPos, float tileJitter, float translucentDepth, float depth)
+{
+    bool doUnderwaterRays = frx_viewFlag(FRX_CAMERA_IN_WATER) && translucentDepth >= depth && frx_worldFlag(FRX_WORLD_HAS_SKYLIGHT);
+    vec3 unit = normalize(modelPos);
+    float scatter = dot(unit, frx_skyLightVector());
+
+    scatter = 0.5 - abs(scatter - 0.5);
+    scatter *= 2.0;
+
+    if (!doUnderwaterRays || scatter <= 0.0) {
+        return a;
+    }
+
+    const float sample = 2.0;
+    const int maxSteps = 10;
+    const float range = 10.0;
+    const float beamL = 3.;
+    const float basePower = 0.25;
+    const float deadRadius = 4.0;
+
+    vec3 ray = frx_cameraPos();
+    vec3 march = unit * sample;
+    float maxDist = length(modelPos);
+
+    ray += tileJitter * march + unit * deadRadius;
+
+    float power = 0.0;
+    float traveled = tileJitter * sample + deadRadius;
+    int steps = 0;
+
+    while (traveled < maxDist && steps < maxSteps) {
+        float e = 0.0;
+
+        e = caustics(ray);
+        e = pow(e, 30.0);
+        // e *= traveled / range;
+
+    #ifdef SHADOW_MAP_PRESENT
+        vec4 ray_shadow = (frx_shadowViewMatrix() * vec4(ray - frx_cameraPos(), 1.0));
+        e *= simpleShadowFactor(sshadow, ray_shadow);
+    #endif
+
+        power += e;
+        ray += march;
+        traveled += sample;
+        steps ++;
+    }
+
+    power = power * sample / float(maxSteps) * scatter * basePower;
+
+    vec3 scatterColor = atmos_hdrFogColorRadiance(unit);
+
+    scatterColor /= l2_max3(scatterColor);
+
+    float scatterLuminance = frx_luminance(scatterColor);
+
+    scatterColor = scatterLuminance == 0.0 ? vec3(1.0) : scatterColor / scatterLuminance;
+
+    a.rgb += atmos_hdrCelestialRadiance() * scatterColor * power;
+    a.a += max(0.0, 1.0 - a.a) * power;
+
+    return a;
+}
--- assets/lumi/shaders/post/common/shading.glsl
+++ assets/lumi/shaders/post/common/shading.glsl
@@ -10,15 +10,17 @@
 #include lumi:shaders/common/shadow.glsl
 #include lumi:shaders/common/userconfig.glsl
 #include lumi:shaders/func/glintify2.glsl
-#include lumi:shaders/func/tonemap.glsl
 #include lumi:shaders/func/pbr_shading.glsl
+#include lumi:shaders/func/tile_noise.glsl
+#include lumi:shaders/func/tonemap.glsl
+#include lumi:shaders/func/volumetrics.glsl
 #include lumi:shaders/lib/bitpack.glsl
 #include lumi:shaders/lib/block_dir.glsl
+#include lumi:shaders/lib/caustics.glsl
 #include lumi:shaders/lib/celest_adapter.glsl
 #include lumi:shaders/lib/puddle.glsl
 #include lumi:shaders/lib/rectangle.glsl
 #include lumi:shaders/lib/taa_jitter.glsl
-#include lumi:shaders/func/tile_noise.glsl
 #include lumi:shaders/lib/util.glsl
 #include lumi:shaders/post/common/bloom.glsl
 #include lumi:shaders/post/common/fog.glsl
@@ -214,88 +216,6 @@ vec4 fog(float skyLight, vec4 a, vec3 modelPos, vec3 worldPos, inout float bloom
     return mix(a, fogColor, fogFactor);
 }
 
-float caustics(vec3 worldPos)
-{
-    // turns out, to get accurate coords, a global y-coordinate of water surface is required :S
-    // Sea level is used for the time being..
-    // TODO: might need to prevent division by 0 ?
-    float animator = frx_renderSeconds() * 0.5;
-    vec2 animatonator = frx_renderSeconds() * vec2(0.5, -1.0);
-    vec3 pos = vec3(worldPos.xz + animatonator, animator);
-
-    pos.xy += (SEA_LEVEL - worldPos.y) * frx_skyLightVector().xz / frx_skyLightVector().y;
-
-    float e = cellular2x2x2(pos).x;
-
-    e = smoothstep(-1.0, 1.0, e);
-
-    return e;
-}
-
-vec4 underwaterLightRays(vec4 a, vec3 modelPos, float translucentDepth, float depth)
-{
-    bool doUnderwaterRays = frx_viewFlag(FRX_CAMERA_IN_WATER) && translucentDepth >= depth && frx_worldFlag(FRX_WORLD_HAS_SKYLIGHT);
-    vec3 unit = normalize(modelPos);
-    float scatter = dot(unit, frx_skyLightVector());
-
-    scatter = 0.5 - abs(scatter - 0.5);
-    scatter *= 2.0;
-
-    if (!doUnderwaterRays || scatter <= 0.0) {
-        return a;
-    }
-
-    const float sample = 2.0;
-    const int maxSteps = 10;
-    const float range = 10.0;
-    const float beamL = 3.;
-    const float basePower = 0.25;
-    const float deadRadius = 4.0;
-
-    vec3 ray = frx_cameraPos();
-    vec3 march = unit * sample;
-    float maxDist = length(modelPos);
-
-    ray += tileJitter * march + unit * deadRadius;
-
-    float power = 0.0;
-    float traveled = tileJitter * sample + deadRadius;
-    int steps = 0;
-
-    while (traveled < maxDist && steps < maxSteps) {
-        float e = 0.0;
-
-        e = caustics(ray);
-        e = pow(e, 30.0);
-        // e *= traveled / range;
-
-    #ifdef SHADOW_MAP_PRESENT
-        vec4 ray_shadow = (frx_shadowViewMatrix() * vec4(ray - frx_cameraPos(), 1.0));
-        e *= simpleShadowFactor(u_shadow, ray_shadow);
-    #endif
-
-        power += e;
-        ray += march;
-        traveled += sample;
-        steps ++;
-    }
-
-    power = power * sample / float(maxSteps) * scatter * basePower;
-
-    vec3 scatterColor = atmos_hdrFogColorRadiance(unit);
-
-    scatterColor /= l2_max3(scatterColor);
-
-    float scatterLuminance = frx_luminance(scatterColor);
-
-    scatterColor = scatterLuminance == 0.0 ? vec3(1.0) : scatterColor / scatterLuminance;
-
-    a.rgb += atmos_hdrCelestialRadiance() * scatterColor * power;
-    a.a += max(0.0, 1.0 - a.a) * power;
-
-    return a;
-}
-
 void custom_sky(in vec3 modelPos, in float blindnessFactor, in bool maybeUnderwater, inout vec4 a, inout float bloom_out)
 {
     vec3 worldSkyVec = normalize(modelPos);
@@ -416,7 +336,7 @@ vec4 hdr_shaded_color(
         if (v_blindness == 1.0) return vec4(0.0);
         custom_sky(modelPos, 1.0 - v_blindness, maybeUnderwater, a, bloom_out);
     #ifdef WATER_CAUSTICS
-        a = underwaterLightRays(a, modelPos, translucentDepth, depth);
+        a = underwaterLightRays(u_shadow, a, modelPos, tileJitter, translucentDepth, depth);
     #endif
         // mark as managed draw, vanilla sky is an exception
         return vec4(a.rgb * 1.0 - v_blindness, 1.0);
@@ -522,7 +442,7 @@ vec4 hdr_shaded_color(
     }
 
 #ifdef WATER_CAUSTICS
-    a = underwaterLightRays(a, modelPos, translucentDepth, depth);
+    a = underwaterLightRays(u_shadow, a, modelPos, tileJitter, translucentDepth, depth);
 #endif
 
     return a;
