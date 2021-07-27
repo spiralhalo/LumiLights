209b60b164d87562ffba60202b949ec0905eeb3d
spiralhalo <re.nanashi95@gmail.com>
2021-07-28 06:26:09 +0700

    Tone down light rays strength at noon

--- assets/lumi/shaders/func/volumetrics.glsl
+++ assets/lumi/shaders/func/volumetrics.glsl
@@ -23,7 +23,7 @@ vec4 celestialLightRays(sampler2DArrayShadow sshadow, vec3 modelPos, float expos
 		scatter = 0.5 - abs(scatter - 0.5);
 		scatter *= 2.0;
 	} else { 
-		float sunHorizon = smoothstep(1.5, 0.0, frx_skyLightVector().y);
+		float sunHorizon = 0.2 + 0.8 * l2_clampScale(0.5, 0.0, frx_skyLightVector().y);
 
 		scatter = smoothstep(-1.0, 0.5, scatter) * sunHorizon;
 
